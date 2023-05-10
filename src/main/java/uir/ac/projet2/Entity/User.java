8bf36f6522696866e68b076cdda6966b0c6a6b49
Youssef-Yb07 <ybyhb07@gmail.com>
2023-05-10 09:46:09 +0100

    Packages

---
+++ src/main/java/uir/ac/projet2/Entity/User.java
@@ -0,0 +1,15 @@
+package uir.ac.projet2.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUser;
+
+
+}
